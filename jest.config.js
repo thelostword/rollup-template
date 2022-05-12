ad5a1ec7ab4d736e036edc485c68aa1387f555f8
thelostword <thelostword@163.com>
2022-05-12 12:13:35 +0800

    fix: eslint fix

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: losting
+ * @Date: 2022-04-01 18:06:21
+ * @LastEditTime: 2022-05-12 12:12:45
+ * @LastEditors: losting
+ * @Description:
+ * @FilePath: \rollup-template\jest.config.js
+ */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
